05ab4a306cee75be16c40ed1c12706db7f6624e0
parvizqasimov <119752037+parvizqasimov@users.noreply.github.com>
2022-12-08 23:59:12 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Proje3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+            
+        {
+            Console.WriteLine("Ededleri daxil edin");
+            int a = Convert.ToInt32(Console.ReadLine());    
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = (a+b)/2;
+            Console.WriteLine(c);
+        }
+    }
+}
